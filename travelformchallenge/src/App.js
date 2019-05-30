21b13b2409947b021676cc56b51eff3aeb5b8a5c
Talal Jawaid <talaljawaid916@gmail.com>
2019-05-30 16:25:38 -0700

    Nearly working, just need to add dietary restrictions display

--- travelformchallenge/src/App.js
+++ travelformchallenge/src/App.js
@@ -21,16 +21,22 @@ class App extends Component {
         this.state = {
             firstName: "",
             lastName: "",
-            age: null,
-            gender: null,
+            age: "",
+            gender: "",
             destination: "",
-            dietaryRestrictions:""
+            dietaryRestrictions:"",
+            isDisabled: true 
         }
         this.handleChange = this.handleChange.bind(this);
     }
     
     handleChange(event) {
-        const {name,value,type,checked} = event.target
+        const {name,value,type,checked} = event.target;
+        type === "checkbox" ? 
+        this.setState({
+            [name]: checked
+        })
+        :
         this.setState({
             [name]: value
         })
@@ -39,36 +45,158 @@ class App extends Component {
         return (
             <main>
                 <form>
+                    <div className ="identifyingFields">
+                    <div>
+                        <h2>Trip Planner!</h2>
                     <input 
-                        type="text"
-                        name = "firstName"
-                        placeholder="First Name" 
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        />
-                        
-                        
-                        <br />
-                    <input placeholder="Last Name" /><br />
-                    <input placeholder="Age" /><br />
+                    type="text"
+                    name = "firstName"
+                    placeholder="First Name" 
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
+                    />
                     
-                    {/* Create radio buttons for gender here */}
+                    </div>
+                        
                     <br />
+                    <div>
+                    <input 
+                    type="text"
+                    name = "lastName"
+                    placeholder="Last Name"
+                    value={this.state.lastName}
+                    onChange = {this.handleChange}
+                    />
+                    </div>
+
+                    <br />
+                    <div>
+                    <input 
+                    type="number"
+                    name="age"
+                    placeholder="Age"
+                    value={this.state.age}
+                    onChange={this.handleChange}
+                     />
+                     </div>
+                    </div>
                     
+                    <br/>
+
+                    <div>
+                    {/* Create radio buttons for gender here */}
+                    <input 
+                    type="radio"
+                    name="gender"
+                    value="Male"
+                    checked={this.state.gender === "Male"}
+                    onChange={this.handleChange}
+                    />
+                    Male
+                  
+
+                    <p/>
+                
+                    <input 
+                    type="radio"
+                    name="gender"
+                    value="Female"
+                    checked = {this.state.gender === "Female"}
+                    onChange={this.handleChange}
+                    />
+                    Female
+                    </div>
+
+
+                    
+                    <br/>
+                    <div>
+
                     {/* Create select box for location here */}
+                    <select
+                        value={this.state.destination}
+                        onChange = {this.handleChange}
+                        name = "destination" >
+                        <option value = "Seattle">Seattle</option>
+                        <option value = "Sacramento">Sacramento</option>
+                        <option value = "Atlanta">Atlanta</option>
+                        <option value = "New York">New York</option>
+                        </select>
+                
+                    </div>
+
                     <br />
                     
                     {/* Create check boxes for dietary restrictions here */}
+                    <div>
+                    <input
+                        type ="checkbox"
+                        name = "isVegan"
+                        checked={this.state.isVegan}
+                        onChange={this.handleChange}
+                    /> Vegan
+                    <br />
+
+                    <input
+                        type ="checkbox"
+                        name = "isKosher"
+                        checked={this.state.isKosher}
+                        onChange={this.handleChange}
+                    /> Kosher
+
+                    <br />
+
+                    <input
+                        type ="checkbox"
+                        name = "isHalal"
+                        checked={this.state.isHalal}
+                        onChange={this.handleChange}
+                    /> Halal
+                    <br />
+
+                    <input
+                        type ="checkbox"
+                        name = "isLactose"
+                        checked={this.state.isLactose}
+                        onChange={this.handleChange}
+                    /> Lactose Intolerant
+
+                    <br />
+
+                    <input
+                        type ="checkbox"
+                        name = "isOther"
+                        checked={this.state.isOther}
+                        onChange={this.handleChange}
+                       
+                        onClick= {() => this.setState({
+                            isDisabled: !this.state.isDisabled
+                        })} 
+
+                    /> 
+
+                    <input
+                        type = "text"
+                        name = "otherRestriction"
+                        placeholder="Other Restriction" 
+                        disabled = {this.state.isDisabled}
+                        
+                        value={this.state.otherRestriction}
+                        onChange={this.handleChange}
+
+                    />
+                    </div>
+
                     <br />
                     
                     <button>Submit</button>
                 </form>
                 <hr />
                 <h2>Entered information:</h2>
-                <p>Your name: {this.state.firstName}</p>
-                <p>Your age: {/* Age here */}</p>
-                <p>Your gender: {/* Gender here */}</p>
-                <p>Your destination: {/* Destination here */}</p>
+                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.destination}</p>
                 <p>
                     Your dietary restrictions: 
                     {/* Dietary restrictions here, comma separated */}
